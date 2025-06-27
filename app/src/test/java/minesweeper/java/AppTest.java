0c774f6ccd2fa48fd484b5933707c3e663114a23
Payton Chenault <pchenault08@gmail.com>
2025-06-27 04:10:05 -0700

    removed tests

--- app/src/test/java/minesweeper/java/AppTest.java
+++ app/src/test/java/minesweeper/java/AppTest.java
@@ -3,11 +3,4 @@
  */
 package minesweeper.java;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
-class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-    }
-}
+class AppTest {}
